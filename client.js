be2dab4ed353d2e643003769909baf2fbde92607
warlock001 <75542810+warlock001@users.noreply.github.com>
2022-03-11 19:55:48 +0500

    added emit and listners

--- client.js
+++ client.js
@@ -1,29 +1,46 @@
 let io = require("socket.io-client")
-const stream1 = io("http://localhost:3000/stream1");
-stream1.connect();
 
-const stream2 = io("http://localhost:3000/stream2");
-stream2.connect();
+//connecting both the streams
+const stream1 = io.connect("http://localhost:3000/stream1");
+const stream2 = io.connect("http://localhost:3000/stream2");
 
 
-setTimeout(function() {
-    console.log("stream1 id: "+stream1.id);
-    console.log("stream2 id: "+stream2.id);
+
+//listining to the msg sent by stream1.send();
+stream1.on('message', function (message) {
+     console.log(message);
+});
+
+//
+stream2.on('news' /*name of an event*/ , function (data) {
+    console.log(data);
+
+    stream2.emit('confirm', { 
+        status: 'recived' 
+    });
+
+});
+
+
+
+   
+// setTimeout(function() {
+//      stream1.disconnect();
+//     }, 5000);
+
     
-   }, 2000);
-
-
-setTimeout(function() {
-     stream1.disconnect();
-    }, 5000);
-
+    //    setTimeout(function() {
+    //     console.log(stream1.connected);
+    //     }, 7000);
     
-       setTimeout(function() {
-        console.log(stream1.connected);
-        }, 7000);
-    
-        setTimeout(function() {
-            stream1.connect();
-           }, 5000);
+
+        
+        // setTimeout(function() {
+        //     stream1.connect(); 
+        //    }, 12000);
+
        
-           
+//     setTimeout(function() {
+//     console.log("stream1 id: "+stream1.id);
+//     console.log("stream2 id: "+stream2.id);
+//    }, 17000);
--- server.js
+++ server.js
@@ -11,28 +11,37 @@ var server = app.listen(port, async () => {
   
   var io = socket(server);
 
+
+
    var stream1 = io.of("/stream1");
 
    stream1.on("connection" , (server)=>{
-     console.log("user connected on stream 1");
-
-     server.on('disconnect', () => {
+      console.log("user connected on stream 1");
+      stream1.send('hi');
+      server.on('disconnect', () => {
       console.log('user disconnected from stream 1');
     });
-
-
    })
 
-
-
+  
+ 
     
 var stream2 = io.of("/stream2");
 
     stream2.on("connection" , (server)=>{
-    console.log("user connected on stream2");
+        console.log("user connected on stream2");
 
-        server.on('disconnect', () => {
-        console.log('user disconnected from stream 2');
+        stream2.emit('news', { 
+          hello: 'world' 
+         }); 
+
+         stream2.on('confirm', function(data){
+          console.log(data)
+          })
+  
+
+      server.on('disconnect', () => {
+      console.log('user disconnected from stream 2');
     });
         
     })
